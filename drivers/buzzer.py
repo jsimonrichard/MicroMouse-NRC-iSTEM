35048a6e7dd1374d42257a9eb521269df8b0a288
jsimonrichard <jsimonrichard@gmail.com>
2021-02-25 22:59:23 -0500

    :pencil2: Basic buzzer driver

--- drivers/buzzer.py
+++ drivers/buzzer.py
@@ -1,3 +1,18 @@
+from machine import Pin, PWM
+from utime import sleep_ms
+
 class Buzzer:
-    def __init__(self, pin):
-        self.pin = pin
+    def __init__(self, pin, volume):
+        self._buzzer = PWM(Pin(pin))
+        self._duty_level = volume*1000
+
+    def _play_sound(self, freq, ms):
+        if freq:
+            self._buzzer.freq(freq)
+            self._buzzer.duty_u16(self._duty_level)
+        sleep_ms(ms)
+        self._buzzer.duty_u16(0)
+
+    def play(self, song):
+        for freq, ms in song:
+            self._play_sound(freq, ms)

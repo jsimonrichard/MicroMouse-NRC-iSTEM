f613c574669b40701039f2f74dc335eaf4f381b0
jsimonrichard <jsimonrichard@gmail.com>
2021-02-25 22:30:41 -0500

    :pencil2: Basic button driver

--- drivers/button.py
+++ drivers/button.py
@@ -1,3 +1,9 @@
+from machine import Pin
+
 class Button:
     def __init__(self, pin):
-        self.pin = pin
+        self._pin = Pin(pin, Pin.IN)
+
+    @property
+    def isPressed(self):
+        return self._pin.value()

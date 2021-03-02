1ea57d4b822d45648e9be49f212d271d555b7af9
jsimonrichard <jsimonrichard@gmail.com>
2021-03-01 22:05:26 -0500

    :wrench: Update pins

--- config/pins.py
+++ config/pins.py
@@ -1,23 +1,38 @@
 """
 This is the file for GPIO pin configuration.
-
-(WIP)
 """
 
-BUTTON_PIN = 0
-BUZZER_PIN = 0
+BUTTON_PIN = 22
+BUZZER_PIN = 21
+
+DHT_PIN = 20
 
 MOTOR_PINS = {
-    "left": 0,
-    "right": 0
+    "left": {
+        "forward": 6,
+        "backward": 7
+    },
+    "right": {
+        "forward": 8,
+        "backward": 9
+    }
 }
 
 PING_PINS = {
-    "trig": 0,
-    "echo": {
-        "front": 0,
-        "back": 0,
-        "left": 0,
-        "right": 0
+    "front": {
+        "trig": 12,
+        "echo": 13
+    },
+    "back": {
+        "trig": 16,
+        "echo": 17
+    },
+    "left": {
+        "trig": 14,
+        "echo": 15
+    },
+    "right": {
+        "trig": 18,
+        "echo": 19
     }
 }

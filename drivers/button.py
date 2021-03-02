50a21a653e951fdfb0d575bda35b1b425b7c350a
jsimonrichard <jsimonrichard@gmail.com>
2021-03-01 22:05:49 -0500

    :bug: Button pin must be pulled down

--- drivers/button.py
+++ drivers/button.py
@@ -2,7 +2,7 @@ from machine import Pin
 
 class Button:
     def __init__(self, pin):
-        self._pin = Pin(pin, Pin.IN)
+        self._pin = Pin(pin, Pin.IN, Pin.PULL_DOWN)
 
     @property
     def isPressed(self):

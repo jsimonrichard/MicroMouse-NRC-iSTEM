ae9a6a4dd019d7fb2dc53e099a09b8b9be2661e0
jsimonrichard <jsimonrichard@gmail.com>
2021-03-13 14:04:39 -0500

    :bomb: Add robot arguments, dht sensor not working

--- _main.py
+++ _main.py
@@ -5,6 +5,7 @@ Entrypoint for the project
 
 from machine import Pin
 import utime
+import dht
 
 from config.pins import BUTTON_PIN, BUZZER_PIN, MOTOR_PINS, PING_PINS
 from config.maze import UNIT_SIZE, MAZE_SIZE, SOLUTIONS
@@ -23,11 +24,12 @@ led.high()
 btn = button.Button(BUTTON_PIN)
 bzz = buzzer.Buzzer(BUZZER_PIN, 1)
 
-# Update this
 motors = motor.TwoWheel(MOTOR_PINS)
 
-ping_sensors = map(ping.PingSensor, PING_PINS)
+ping_sensors = list(map(ping.PingSensor, PING_PINS))
 ping_collection = ping.PingCollection(ping_sensors)
+
+dht_sensor = dht.DHT11(Pin(20))
 
 # MAZE INITIALIZATION
 maze = Maze(MAZE_SIZE)
@@ -39,12 +41,14 @@ robot = Robot(
     bzz,
     motors,
     ping_collection,
-    maze
+    dht_sensor,
+    maze,
+    UNIT_SIZE
 )
 
 utime.sleep_ms(100)
 led.low()
 print("[PICO] Initialized")
 
-robot.Start(no_peripherals=True)
+robot.Start()
 

e33fe7373b153b7b031ea8fb4c39c48fea8e396a
jsimonrichard <jsimonrichard@gmail.com>
2021-03-13 11:37:40 -0500

    :recycle: refactor names, add Win song

--- config/songs.py
+++ config/songs.py
@@ -10,20 +10,26 @@ ON = [
     (988,250)  # B5 for 0.25 seconds
 ]
 
-START_CRAWL = [
+CRAWL = [
     (988,250) # B5 for 0.25 seconds
 ]
 
-START_HOMING = [
+HOMING = [
     (988,250), # B5 for 0.25 seconds
     (659,250), # E5 for 0.25 seconds
     (494,250)  # B4 for 0.25 seconds
 ]
 
-START_DASH = [
+DASH = [
     (494,250), # B4 for 0.25 seconds
     (0,2),     # Break
     (494,250), # B4 for 0.25 seconds
     (0,2),     # Break
     (659,500)  # E5 for 0.5 seconds
+]
+
+WIN = [
+    (659,250), # E5 for 0.25 seconds
+    (0,2), # Break
+    (659,250) 
 ]

d0b23af4dadf6009c2ce0b654b21b7c8e1a2010a
jsimonrichard <jsimonrichard@gmail.com>
2021-03-22 17:36:02 -0400

    :white_check_mark: Add rotation test

---
+++ rot_test.py
@@ -0,0 +1,32 @@
+"""
+Test for the motors
+"""
+
+from machine import Pin
+from utime import sleep_ms
+
+from config import AXIL_LENGTH
+from config.pins import MOTOR_PINS, BUTTON_PIN
+
+from drivers.motor import TwoWheel
+from drivers.button import Button
+
+led = Pin(25, Pin.OUT)
+led.high()
+
+btn = Button(BUTTON_PIN)
+while not btn.isPressed:
+    ...
+led.low()
+sleep_ms(1000)
+
+speed = 30000
+motors = TwoWheel(MOTOR_PINS, AXIL_LENGTH)
+
+print("Let's goooooooo")
+
+motors.rot(-speed, 0)
+sleep_ms(163)
+motors.stop()
+
+print("Yeah!!!!!")
